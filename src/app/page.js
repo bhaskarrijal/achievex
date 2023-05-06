a651d5d3c0c2360f6071e745b8065dd6a4a11a2d
Bhaskar Rijal <bhaskarrijal13@gmail.com>
2023-05-07 01:09:52 +0545

    fixes

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import { useAuthContext } from "@/context/AuthContext"
 import { useData } from "@/context/DataContext"
 import { useRouter } from "next/navigation"
 import React, { useEffect } from "react"
+import bg from "../../assets/bg.jpg"
+import Link from "next/link"
 
 export default function Home() {
     const { user } = useAuthContext()
@@ -15,7 +17,20 @@ export default function Home() {
 
     return (
         <>
-            Not logged in
+            <div
+                className="container flex flex-col items-center justify-center w-full h-[480px] gap-5 bg-cover bg-top bg-no-repeat"
+                style={
+                    {
+                        backgroundImage: `url(${bg.src})`,
+                    }
+                }>
+                <h1 className='text-5xl font-bold text-primary'>WELCOME TO ACHIEVEX!</h1>
+                <Link href='/signup'>
+                    <button className='px-4 py-2 mt-4 text-base uppercase bg-white rounded-lg text-primary hover:bg-white/90'>
+                        Get Started
+                    </button>
+                </Link>
+            </div>
         </>
     );
 }
